c99ca22871f8fbb5a4bdf63a3899a93c1b5083ec
Sonia <soniacocamartin@gmail.com>
2018-02-14 11:14:19 +0100

    cambio 1

--- AcademiaAlways/AcademiaAlways/Gestiones.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class Gestiones
-
-    Private Sub btVolver_Click(sender As Object, e As EventArgs) Handles btVolver.Click
-        IniciarSesion.Show()
-        Me.Close()
-    End Sub
-End Class
